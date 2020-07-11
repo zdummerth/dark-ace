a4b68dae766b543d7dabbb1a2291733f91ea7657
Zach Dummerth <zdummerth@gmail.com>
2020-07-11 00:58:58 -0500

    works okay

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -1,10 +1,42 @@
 import React, { useState, useEffect} from "react"
 import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 import { RiArrowRightLine } from "react-icons/ri"
 import { RiArrowLeftLine } from "react-icons/ri"
 //import PropTypes from 'prop-types'
 
+const SlideshowContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid #C00A0A;
+  margin: 30px auto;
+  max-width: 800px;
+  height: 80vh;
+  overflow: hidden;
+`
+const ImgContainer = styled.div`
+  // height: 50%;
+`
+const SlideshowControls = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 80%;
+  margin: 5px auto;
+`
+const SlideshowButton = styled.button`
+  display: flex;
+  background: none;
+  border: none;
+  outline: none;
+  font-size: 40px;
+  color: #C00A0A;
+  :hover {
+      cursor: pointer;
+  }
+`
 const SlideShow = () => {
   const data = useStaticQuery(graphql`
   query {
@@ -13,7 +45,7 @@ const SlideShow = () => {
         node {
           name
           childImageSharp {
-            fluid(maxWidth: 200) {
+            fluid(maxWidth: 800) {
               ...GatsbyImageSharpFluid
             }
           }
@@ -23,10 +55,9 @@ const SlideShow = () => {
   }
 `)
 
-  console.log({data})
   const images = data.allFile.edges.map(({ node }) => node)
   const [index, setIndex] = useState(0);
-  const { fluid } = images[index].childImageSharp
+  // const { fluid } = images[index].childImageSharp
 
   const handleNext = () =>
     index === images.length - 1 ? setIndex(0) : setIndex(index + 1)
@@ -58,23 +89,25 @@ const SlideShow = () => {
   })
 
   return(
-    <div className='slideshow-container'>
-        <h1>SlideShow</h1>
-        <div className="main-container">
-          <Img
-              fluid={fluid}
-              alt={'slideshow for feature images'}
-              fadeIn={true}
-          />
-          <div className="slideshow-controls">
-            <button className='slideshow-button' onClick={handlePrevious}><RiArrowLeftLine/></button>
-            <div className='featured-thumbs-container'>
-              {thumbs}
-            </div>
-            <button className='slideshow-button' onClick={handleNext}><RiArrowRightLine/></button>
-          </div>
-        </div>
-    </div>
+    <>
+      <h1>SlideShow</h1>
+      <SlideshowContainer >
+            {/* <ImgContainer> */}
+              <Img
+                  fluid={images[index].childImageSharp.fluid}
+                  alt={'slideshow for feature images'}
+                  fadeIn={true}
+              />
+            {/* </ImgContainer> */}
+            <SlideshowControls>
+              <SlideshowButton className='slideshow-button' onClick={handlePrevious}><RiArrowLeftLine/></SlideshowButton>
+              <div className='featured-thumbs-container'>
+                {thumbs}
+              </div>
+              <SlideshowButton className='slideshow-button' onClick={handleNext}><RiArrowRightLine/></SlideshowButton>
+            </SlideshowControls>
+      </SlideshowContainer>
+    </>
   )
 }
 
