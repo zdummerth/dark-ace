8a3824f7627a1dbea6220a71b368c7f2daf69192
Zach Dummerth <zdummerth@gmail.com>
2020-07-11 22:20:28 -0500

    got basic slideshow up

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -4,31 +4,28 @@ import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import { RiArrowRightLine } from "react-icons/ri"
 import { RiArrowLeftLine } from "react-icons/ri"
-//import PropTypes from 'prop-types'
 
 const SlideshowContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  border: 1px solid #C00A0A;
+  // justify-content: space-between;
+  // border: 1px solid #C00A0A;
   margin: 30px auto;
   max-width: 800px;
   height: 80vh;
-  overflow: hidden;
+  img {
+    // border: 3px solid blue;
+  }
 `
-const ImgContainer = styled.div`
-  // height: 50%;
-`
-const StyledImg = styled(Img)`
-  max-height: 80%;
-`
+
+
 const SlideshowControls = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
   width: 80%;
   margin: 5px auto;
-  height: 20%;
+  // height: 20%;
 `
 const SlideshowButton = styled.button`
   display: flex;
@@ -41,16 +38,50 @@ const SlideshowButton = styled.button`
       cursor: pointer;
   }
 `
+const ThumbsContainer = styled.div`
+  display: flex;
+`
+const ThumbWrapper = styled.div`
+  position: relative;
+  height: 50px;
+  width: 50px;
+  overflow: hidden;
+  margin-left: 1.5px;
+  margin-right: 1.5px;
+  :hover {
+      cursor: pointer;
+  }
+  @media (max-width: 786px) {
+      height: 30px;
+      width: 30px;
+  }
+`
+const Overlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 2;
+`
+
 const SlideShow = () => {
   const data = useStaticQuery(graphql`
   query {
     allFile(filter: {relativeDirectory: {eq: "slideshow"}}) {
       edges {
         node {
-          name
-          childImageSharp {
+          full: childImageSharp {
             fluid(maxWidth: 800) {
               ...GatsbyImageSharpFluid
+            }
+          }
+          thumb: childImageSharp {
+            fixed(width: 50, height: 50) {
+              ...GatsbyImageSharpFixed
             }
           }
         }
@@ -59,36 +90,34 @@ const SlideShow = () => {
   }
 `)
 
-  const images = data.allFile.edges.map(({ node }) => node)
+  const fullImages = data.allFile.edges.map(({ node }) => node.full.fluid)
+  const thumbImages = data.allFile.edges.map(({ node }) => node.thumb.fixed)
   const [index, setIndex] = useState(0);
-  // const { fluid } = images[index].childImageSharp
 
   const handleNext = () =>
-    index === images.length - 1 ? setIndex(0) : setIndex(index + 1)
+    index === fullImages.length - 1 ? setIndex(0) : setIndex(index + 1)
   const handlePrevious = () =>
-    index === 0 ? setIndex(images.length - 1) : setIndex(index - 1)
+    index === 0 ? setIndex(fullImages.length - 1) : setIndex(index - 1)
 
   // useEffect(() => {
   //   const timer = setInterval(() => {
   //     handleNext()
   //   }, 3000); //duration
   //   return () => clearInterval(timer); //cleanup
-  // }, [index, images, handleNext]); //compare
+  // }, [handleNext]);
 
-  const thumbs = images.map((img, ind) => {
-    const overlay = index === ind ? 'overlay hidden' : 'overlay'
+  const thumbs = thumbImages.map((img, ind) => {
     return (
-      <div 
-        className="featured-thumb-container"
+      <ThumbWrapper 
         onClick={() => setIndex(ind)}
         key={ind}
         >
           <Img
-            fluid={img.childImageSharp.fluid}
+            fixed={img}
             alt={'thumbnail for featured images slideshow'}
           />
-          <div className={overlay}></div>
-      </div>
+          {index === ind ? null : <Overlay />}
+      </ThumbWrapper>
     )
   })
 
@@ -96,18 +125,17 @@ const SlideShow = () => {
     <>
       <h1>SlideShow</h1>
       <SlideshowContainer >
-            {/* <ImgContainer> */}
-              <StyledImg
-                  fluid={images[index].childImageSharp.fluid}
-                  alt={'slideshow for feature images'}
-                  fadeIn={true}
-              />
-            {/* </ImgContainer> */}
+            <Img
+                fluid={fullImages[index]}
+                alt={'slideshow for feature images'}
+                fadeIn={true}
+                style={{maxHeight: '80%'}}
+            />
             <SlideshowControls>
-              <SlideshowButton className='slideshow-button' onClick={handlePrevious}><RiArrowLeftLine/></SlideshowButton>
-              <div className='featured-thumbs-container'>
+              <SlideshowButton onClick={handlePrevious}><RiArrowLeftLine/></SlideshowButton>
+              <ThumbsContainer>
                 {thumbs}
-              </div>
+              </ThumbsContainer>
               <SlideshowButton className='slideshow-button' onClick={handleNext}><RiArrowRightLine/></SlideshowButton>
             </SlideshowControls>
       </SlideshowContainer>
