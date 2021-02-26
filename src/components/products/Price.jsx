3356fcbe6d13857a6c832d0df854a72e1465142c
zach <zdummerth@gmail.com>
2021-02-26 12:34:49 -0600

    add price file

---
+++ src/components/products/Price.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+import { formatPrice } from '../../utils/helpers'
+import { colors } from '../../utils/styles';
+
+const BasicPrice = styled.div`
+  font-size: 1.75rem;
+  font-weight: bold;
+  margin-top: 0;
+`
+const CompareAtPriceWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+const CompareAtPrice = styled.div`
+  position: relative;
+  font-size: 1.75rem;
+  margin-right: 20px;
+
+  .line-through {
+    position: absolute;
+    border-top: 3.5px solid ${colors.brand};
+    width: 100%;
+    transform: rotate(-10deg);
+    top: 50%;
+  }
+`
+const NewPrice = styled.div`
+  -webkit-text-stroke: 1px ${colors.brand};
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: ${colors.lightest};
+  text-shadow:
+  -1px -1px 0 ${colors.brand},  
+  1px -1px 0 ${colors.brand},
+  -1px 1px 0 ${colors.brand},
+    1px 1px 6px ${colors.lightest};
+`
+
+const Price = ({ price, compareAtPrice }) => {
+
+
+  return compareAtPrice ? (
+    <>
+      <CompareAtPriceWrapper>
+        <CompareAtPrice>
+          <div>{formatPrice(compareAtPrice)}</div>
+          <div className="line-through"></div>
+        </CompareAtPrice>
+        <NewPrice>{formatPrice(price)}</NewPrice>
+      </CompareAtPriceWrapper>
+    </>
+  ) : (
+      <BasicPrice>{formatPrice(price)}</BasicPrice>
+    )
+}
+
+
+export default Price
+
