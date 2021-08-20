a4ed867e3d046c060d8981ccbf1e041c77cacbde
zach dummerth <zdummerth@gmail.com>
2021-08-20 11:56:22 -0500

    remove console log

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
 
-  console.log('the products handles', productPageResult)
+  // console.log('the products handles', productPageResult)
 
   productPageResult.data.allShopifyProduct.edges.forEach(({ node }) => {
     createPage({
